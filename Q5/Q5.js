3703c9d9e7b7a01a5ad72450a8e62722eef290b0
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-09 15:32:22 +0500

    Add files via upload

---
+++ Q5/Q5.js
@@ -0,0 +1,3 @@
+var famous_person = "Todd Kashdan";
+var message = famous_person + " said, “When we are open to new possibilities, we find them. Be open and skeptical of everything.”";
+console.log(message);
