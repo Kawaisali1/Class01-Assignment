da74499912ccf9885ea80bfee893b3f38db2ba08
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-09 18:23:23 +0500

    Add files via upload

---
+++ Q13/Q13.js
@@ -0,0 +1,5 @@
+var trans = ["Honda", "Yamaha", "Hi Speed", "Toyota"];
+console.log("I want to buy " + trans[0] + " CD70 motorcycle");
+console.log("but I can not afford it now");
+console.log("At this time I own " + trans[2]);
+console.log("I am planning to buy " + trans[1] + " and " + trans[3] + " in future");
