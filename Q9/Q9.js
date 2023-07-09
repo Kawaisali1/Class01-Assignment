eb501191b41e69acb5f8548fce80dc22edb12c77
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-09 16:16:11 +0500

    Add files via upload

---
+++ Q9/Q9.js
@@ -0,0 +1,3 @@
+var fav_num = 9;
+var str = "My favorite number is ";
+console.log(str + fav_num);
---
+++ Q9/Q9.ts
@@ -0,0 +1,4 @@
+ var fav_num = 9;
+ var str = "My favorite number is ";
+
+ console.log(str + fav_num);
