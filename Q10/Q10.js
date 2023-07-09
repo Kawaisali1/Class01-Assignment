3543dc49cfcecfa5e9521dede6df169b502bd6ec
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-09 16:23:14 +0500

    Add files via upload

---
+++ Q10/Q10.js
@@ -0,0 +1,7 @@
+// code from Q9
+// identifier name 
+var fav_num = 9;
+// string name
+var str = "My favorite number is ";
+// I found that there are two ways to concatenate: (i) by comma (,) and (ii) by plus (+)
+console.log(str + fav_num);
