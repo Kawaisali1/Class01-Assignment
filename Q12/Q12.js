cbaaccf7e841f8d515cd7fa5f4c72e54f0512abc
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-09 18:07:08 +0500

    Add files via upload

---
+++ Q12/Q12.js
@@ -0,0 +1,10 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+print a message to them. The text of each message should be the same, but each message should be personalized
+with the person’s name.*/
+var st = ["Zubair Ali", "Farooq Brohi", "Farooq Nindwani", "Awais Pirzado", "Adil Brohi"];
+var str = "Hello, How are you, ";
+console.log(str + st[0] + "?");
+console.log(str + st[1] + "?");
+console.log(str + st[2] + "?");
+console.log(str + st[3] + "?");
+console.log(str + st[4] + "?");
