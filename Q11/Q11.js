882b31448803b214ab599c412a79ecea40fb8e2f
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-09 17:17:29 +0500

    Add files via upload

---
+++ Q11/Q11.js
@@ -0,0 +1,8 @@
+/*Names: Store the names of a few of your friends in a array called names. Print each person’s name by
+accessing each element in the list, one at a time.*/
+var st = ['Zubair Daudpoto', 'Farooq Brohi', 'Farooq Nindwani', 'Awais Pirzado', 'Adil Brohi'];
+console.log("\t", st[0]);
+console.log("\t", st[1]);
+console.log("\t", st[2]);
+console.log("\t", st[3]);
+console.log("\t", st[4]);
