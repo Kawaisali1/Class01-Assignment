9ff1c861f3b10182abd85d6192ed09dc93939fa7
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-11 18:33:28 +0500

    Add files via upload

---
+++ Q15/Q15.ts
@@ -0,0 +1,22 @@
+/* Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a 
+new set of invitations. You’ll have to think of someone else to invite.*/
+
+var sr = ["Zubair", "Farooq", "Farooq", "Awais", "Zeeshan", "Adil", "Ahtishan"];
+
+for (let i = 0; i <= 6; i++ ){
+    console.log(sr[i] + ", the dinning time is 8:00 PM. I would like you to be there on mentioned venue.");
+    
+}
+
+console.log("\n\n");
+
+console.log(sr[0] + " would not be able to make it to the dinner.");
+
+console.log("\n\n");
+
+sr[0] = "Ahmed Nawaz";
+
+for(let i = 0; i <= 6; i++){
+    console.log(sr[i] + ", the dinning time is 8:00 PM. I would like you to be there on mentioned venue.");
+    
+} 
