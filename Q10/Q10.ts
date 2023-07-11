4cb013753e68bce1653b701340f3cc9f73db42b7
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-11 13:29:06 +0500

    Update Q10.ts

--- Q10/Q10.ts
+++ Q10/Q10.ts
@@ -1,10 +1,10 @@
  // code from Q9
 
  // identifier name 
- var fav_num = 9;
+ let fav_num = 9;
 
  // string name
- var str = "My favorite number is ";
+ let str = "My favorite number is ";
 
  // I found that there are two ways to concatenate: (i) by comma (,) and (ii) by plus (+)
  console.log(str + fav_num);
