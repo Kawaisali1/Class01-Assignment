50bd2f2898d6f79b1a55349c89869423f8d64de0
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-12 20:58:03 +0500

    Add files via upload

---
+++ Q17/Q17.ts
@@ -0,0 +1,40 @@
+/* More Guests: You just found a bigger dinner table, so now more space is available. Think of three more 
+guests to invite to dinner. */
+
+var sr = ["Zubair", "Farooq", "Farooq", "Awais", "Zeeshan", "Adil", "Ahatishan"];
+
+for (let i = 0; i <= sr.length; i++ ){
+    console.log(sr[i] + ", the dinning time is 8:00 PM. I would like you to be there on mentioned venue.");
+    
+}
+
+console.log("\n\n");
+
+console.log(sr[0] + " would not be able to make it to the dinner.");
+
+console.log("\n\n");
+
+sr[0] = "Ahmed Nawaz";
+
+for(let i = 0; i <= sr.length; i++){
+    console.log(sr[i] + ", the dinning time is 8:00 PM. I would like you to be there on mentioned venue.");
+    
+} 
+
+// three more guest to added
+console.log("\nGuys, I have found a bigger dinning table, so I am inviting three more guest.\n\n");
+
+sr = ["Ali Hyder", "Ahmed Nawaz", "Farooq", "Farooq", "Khalid", "Awais", "Zeeshan", "Adil", "Ahatishan"];
+
+sr.push("Atif");
+
+for (let i = 0; i < sr.length; i++){
+console.log(sr[i])
+}
+
+console.log("You all are invited.\n");
+
+for(let i = -5; i <sr.length; i++ ){
+console.log(sr[sr.length - 1] + ", sorry to say I wouldn't be able to invite you.");
+sr.pop();
+}
