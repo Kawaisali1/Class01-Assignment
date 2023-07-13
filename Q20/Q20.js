c390ef760f40ba3469556d23ac3a2cad4abbd638
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-13 14:13:33 +0500

    Add files via upload

---
+++ Q20/Q20.js
@@ -0,0 +1,7 @@
+/* Think of something you could store in a array. For example, you could make a list of mountains, rivers,
+countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these
+items. */
+var aRr = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"];
+for (var i = 0; i < aRr.length; i++) {
+    console.log(i + 1 + ".\t" + aRr[i]);
+}
---
+++ Q20/Q20.ts
@@ -0,0 +1,9 @@
+/* Think of something you could store in a array. For example, you could make a list of mountains, rivers, 
+countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these 
+items. */
+
+let aRr = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"];
+
+for(let i = 0; i < aRr.length; i++){
+    console.log(i + 1 +".\t" + aRr[i]);    
+}
