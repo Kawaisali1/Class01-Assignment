332211b0afc952e20efa56d018f23caa94efd7f2
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-14 17:25:58 +0500

    Q22.js

--- Q22/Q22.js
+++ Q22/Q22.js
@@ -3,19 +3,20 @@ happen. Change an index in one of your programs to produce an index error. Make 
 before closing the program.*/
 /*They think of something you could store in a TypeScript Object. Write a program that creates Objects
 containing these items.*/
-var cities = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"];
-var ob = {
-    a: cities[0],
-    b: cities[1],
-    c: cities[2],
-    d: cities[3],
-    e: cities[4],
-    f: cities[5],
-    // g: cities[y],
+var citiesCopy = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"];
+var obCopy = {
+    a: citiesCopy[0],
+    b: citiesCopy[1],
+    c: citiesCopy[2],
+    d: citiesCopy[3],
+    e: citiesCopy[4],
+    f: citiesCopy[5],
+    // g: citiesCopy[y],
 };
-console.log(ob.a);
-console.log(ob.b);
-console.log(ob.c);
-console.log(ob.d);
-console.log(ob.e);
-console.log(ob.f);
+console.log(obCopy.a);
+console.log(obCopy.b);
+console.log(obCopy.c);
+console.log(obCopy.d);
+console.log(obCopy.e);
+console.log(obCopy.f);
+// console.log(obCopy.g);
