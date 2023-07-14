22bf45af3fdafbc45ef7c584bde0a8fcdc976b4b
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-14 17:58:09 +0500

    Add files via upload

---
+++ Q23/Q23.js
@@ -0,0 +1,42 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your
+prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. */
+var car = 'subaru';
+var num1 = "five";
+var bike = "Honda";
+var num2 = 2;
+var bool1 = true;
+var bool2 = false;
+var qty = 6;
+var num3 = 8;
+var num4 = 10;
+var cls = 7;
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+console.log("Is num1 == 'five'? I predict True.");
+console.log(num1 == 'five');
+console.log("Is bike == 'Honda'? I predict True.");
+console.log(bike = "Honda");
+console.log("Is num2 == 2 ? I predict True.");
+console.log(num2 == 3);
+console.log("Is bool1 == false? I predict false.");
+console.log(bool1 = false);
+console.log("Is bool2 == true? I predict falsw.");
+console.log(bool2 = true);
+console.log("Is qty == 6? I predict True.");
+console.log(qty == 7);
+console.log("Is num3 == 8? I predict True.");
+console.log(num3 == 9);
+console.log("Is num4 == 10? I predict True.");
+console.log(num4 == 0);
+console.log("Is cls == 7? I predict True.");
+console.log(cls == 7);
---
+++ Q23/Q23.ts
@@ -0,0 +1,56 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your 
+prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. */
+
+
+let car = 'subaru';
+let num1 = "five";
+let bike = "Honda";
+let num2 = 2;
+let bool1 = true;
+let bool2= false;
+let qty = 6;
+let num3 = 8;
+let num4 = 10;
+let cls = 7;
+
+
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+console.log("Is num1 == 'five'? I predict True.");
+console.log(num1 == 'five');
+
+console.log("Is bike == 'Honda'? I predict True.");
+console.log(bike = "Honda");
+
+console.log("Is num2 == 2 ? I predict True.");
+console.log(num2 == 3);
+
+console.log("Is bool1 == false? I predict false.");
+console.log(bool1 = false);
+
+console.log("Is bool2 == true? I predict falsw.");
+console.log(bool2 = true);
+
+console.log("Is qty == 6? I predict True.");
+console.log(qty == 7);
+
+console.log("Is num3 == 8? I predict True.");
+console.log(num3 == 9);
+
+console.log("Is num4 == 10? I predict True.");
+console.log(num4 == 0);
+
+console.log("Is cls == 7? I predict True.");
+console.log(cls == 7);
+
