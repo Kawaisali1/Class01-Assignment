728940972c878187b71313ed8141fd4a2b020f67
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-14 17:12:52 +0500

    Add files via upload

---
+++ Q21/Q21.js
@@ -0,0 +1,17 @@
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects
+containing these items.*/
+var cities = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"];
+var ob = {
+    a: cities[0],
+    b: cities[1],
+    c: cities[2],
+    d: cities[3],
+    e: cities[4],
+    f: cities[5],
+};
+console.log(ob.a);
+console.log(ob.b);
+console.log(ob.c);
+console.log(ob.d);
+console.log(ob.e);
+console.log(ob.f);
