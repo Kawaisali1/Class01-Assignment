9af159d6cf396912e9430b68eda9a56cd204eae1
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-14 17:20:21 +0500

    Add files via upload

---
+++ Q22/Q22.js
@@ -0,0 +1,21 @@
+/* Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one
+happen. Change an index in one of your programs to produce an index error. Make sure you correct the error
+before closing the program.*/
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects
+containing these items.*/
+var cities = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"];
+var ob = {
+    a: cities[0],
+    b: cities[1],
+    c: cities[2],
+    d: cities[3],
+    e: cities[4],
+    f: cities[5],
+    // g: cities[y],
+};
+console.log(ob.a);
+console.log(ob.b);
+console.log(ob.c);
+console.log(ob.d);
+console.log(ob.e);
+console.log(ob.f);
---
+++ Q22/Q22.ts
@@ -0,0 +1,23 @@
+/* Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one 
+happen. Change an index in one of your programs to produce an index error. Make sure you correct the error 
+before closing the program.*/
+
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects 
+containing these items.*/
+let cities = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"]
+let ob ={
+    a: cities[0],
+    b: cities[1],
+    c: cities[2],
+    d: cities[3],
+    e: cities[4],
+    f: cities[5],
+    // g: cities[y],
+}
+
+console.log(ob.a);
+console.log(ob.b);
+console.log(ob.c);
+console.log(ob.d);
+console.log(ob.e);
+console.log(ob.f);
