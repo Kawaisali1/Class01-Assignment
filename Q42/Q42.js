df89c152835199d602e73bdbec988bf23e0fea08
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-07-21 13:10:35 +0500

    Update Q42.js

--- Q42/Q42.js
+++ Q42/Q42.js
@@ -1,18 +1,9 @@
 "use strict";
-// /*
-// Great Magicians: Start with a copy of your program from Exercise 41. Write a function called make_great() that
-// modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians()
-// to see that the list has actually been modified.
-// */
-// function show_magicians(){
-//     function make_great(a:String){
-//     let magicianName = ['Harry Houdini', 'Dynamo', 'David Copperfield', 'Criss Angel', 'David Blaine']
-//     for(let i = 0; i < magicianName.length; i++)
-//     console.log(magicianName[i]);
-//         console.log(magicianName.join(a));
-//     }
-// }
-// show_magicians();
+ /*
+Great Magicians: Start with a copy of your program from Exercise 41. Write a function called make_great() that
+modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians()
+to see that the list has actually been modified.
+ */
 function make_great(magicians) {
     for (let i = 0; i < magicians.length; i++) {
         magicians[i] += ", the Great.";
