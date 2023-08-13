ee91dd549501eaee8537a307ee010476acefe332
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-08-13 13:04:00 +0500

    Update Q19.ts

--- Q19/Q19.ts
+++ Q19/Q19.ts
@@ -1,37 +1,12 @@
-/* More Guests: You just found a bigger dinner table, so now more space is available. Think of three more 
-guests to invite to dinner. */
+/*
+Dinner Guests: Working with one of the programs from exercise 14 through 18, print a message indicating the 
+number of people you are inviting to dinner.
 
-var sr = ["Zubair", "Farooq", "Farooq", "Awais", "Zeeshan", "Adil", "Ahatishan"];
+*/
 
-for (let i = 0; i <= sr.length; i++ ){
-    console.log(sr[i] + ", the dinning time is 8:00 PM. I would like you to be there on mentioned venue.");
-    
-}
+let sr4 = ["Zubair", "Farooq", "Farooq", "Awais", "Zeeshan", "Adil", "Ahatishan"];
+sr4.splice(0, 0, "Ahmed Nawaz");
+sr4.unshift("Ali Hyder");
+sr4.push("Atif");
 
-console.log("\n\n");
-
-console.log(sr[0] + " would not be able to make it to the dinner.");
-
-console.log("\n\n");
-
-sr[0] = "Ahmed Nawaz";
-
-for(let i = 0; i <= sr.length; i++){
-    console.log(sr[i] + ", the dinning time is 8:00 PM. I would like you to be there on mentioned venue.");
-    
-} 
-
-// three more guest to added
-console.log("\nGuys, I have found a bigger dinning table, so I am inviting three more guest.\n\n");
-
-sr = ["Ali Hyder", "Ahmed Nawaz", "Farooq", "Farooq", "Khalid", "Awais", "Zeeshan", "Adil", "Ahatishan"];
-
-sr.push("Atif");
-
-for (let i = 0; i < sr.length; i++){
-console.log(sr[i])
-}
-
-console.log("You all are invited.");
-
-console.log("\n\nThe number of people were to be invited was " + sr.length);
+console.log(`\n\tThe total number of people invited to the dinner was ${sr4.length}`);
