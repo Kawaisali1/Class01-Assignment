a2b779974e8f5daa4fe1aed44e9ee9a3949e91e5
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-08-13 12:52:52 +0500

    Update Q14.js

--- Q14/Q14.js
+++ Q14/Q14.js
@@ -1,7 +1,8 @@
+"use strict";
 /*Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that
 includes at least three people you’d like to invite to dinner. Then use your list to print a message to each
 person, inviting them to dinner.*/
-var sr = ["Zubair", "Farooq", "Farooq", "Awais", "Zeeshan", "Adil", "Ahtishan"];
-for (var i = 0; i <= 6; i++) {
+let sr = ["Zubair", "Farooq", "Farooq", "Awais", "Zeeshan", "Adil", "Ahtishan"];
+for (let i = 0; i < sr.length; i++) {
     console.log(sr[i] + ", the dinning time is 8:00 PM. I would like you to be there on mentioned venue.");
 }
