3f7668c9aca9d922192f1fa48931958fa8512c32
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-08-13 13:05:42 +0500

    Update Q20.js

--- Q20/Q20.js
+++ Q20/Q20.js
@@ -1,7 +1,8 @@
+"use strict";
 /* Think of something you could store in a array. For example, you could make a list of mountains, rivers,
 countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these
 items. */
-var aRr = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"];
-for (var i = 0; i < aRr.length; i++) {
+let aRr = ["Karachi", "Berlin", "Sofia", "Veina", "Qatar City", "Dubai"];
+for (let i = 0; i < aRr.length; i++) {
     console.log(i + 1 + ".\t" + aRr[i]);
 }
