fc6aacdcb88749ca91f612f5773c3c4fc0771fb6
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-08-13 13:03:15 +0500

    Update Q18.js

--- Q18/Q18.js
+++ Q18/Q18.js
@@ -1,14 +1,15 @@
-var places = ["Austalia", "Hungery", "Russia", "Oman", "Sri Lanka"];
+"use strict";
+let places = ["Austalia", "Hungery", "Russia", "Oman", "Sri Lanka"];
 console.log(places);
 console.log("\n");
-var placesSliced = places.slice();
-var placesSorted = placesSliced.sort();
+let placesSliced = places.slice();
+let placesSorted = placesSliced.sort();
 console.log(placesSorted);
 console.log("\n");
 console.log("The array is not changed.");
 console.log(places);
 console.log("\n");
-var placesReversed = placesSliced.reverse();
+let placesReversed = placesSliced.reverse();
 console.log(placesReversed);
 console.log("\n");
 console.log("The array is not changed.");
@@ -18,13 +19,13 @@ console.log("Original array reversed.");
 console.log(places.reverse());
 console.log("\n");
 places.reverse();
-for (var i = 0; i < places.length; i++) {
+for (let i = 0; i < places.length; i++) {
     console.log(places[i]);
 }
 console.log("\n");
 console.log(places.sort());
 console.log("\n");
 console.log(places.reverse());
-for (var i = 0; i < places.length; i++) {
+for (let i = 0; i < places.length; i++) {
     console.log(places[i]);
 }
