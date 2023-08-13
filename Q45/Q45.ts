6ff2f3e3f002ed1cd8995b4925e298ab1b118e1d
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-08-13 13:14:08 +0500

    Add files via upload

---
+++ Q45/Q45.ts
@@ -0,0 +1,23 @@
+/*
+Cars: Write a function that stores information about a car in a Object. The function should always receive a 
+manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the 
+function with the required information and two other name-value pairs, such as a color or an optional feature. 
+Print the Object that’s returned to make sure all the information was stored correctly.
+*/
+
+function carDetails(manufacturer:string, modelName:string, color:string, optionalFeature:string){
+    let carObj = {
+        manufacturer,
+        modelName, 
+        color, 
+        optionalFeature
+    }
+    if(manufacturer === "Honda" && modelName === "Civic" && color === "Black" && optionalFeature === "Booster"){
+        console.log("The details of car are");
+         console.log(carObj);
+    }
+    else 
+        console.log("Information didn't match");
+}
+
+carDetails("Honda", "Civic", "Black", "Booster")
