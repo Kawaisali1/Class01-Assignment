1813497e160466e01586ebd24d5ae2a42438dfe6
Kawaisali1 <138242995+Kawaisali1@users.noreply.github.com>
2023-08-13 12:48:14 +0500

    Update Q11.js

--- Q11/Q11.js
+++ Q11/Q11.js
@@ -1,6 +1,7 @@
+"use strict";
 /*Names: Store the names of a few of your friends in a array called names. Print each person’s name by
 accessing each element in the list, one at a time.*/
-var st = ['Zubair Daudpoto', 'Farooq Brohi', 'Farooq Nindwani', 'Awais Pirzado', 'Adil Brohi'];
+let st = ['Zubair Daudpoto', 'Farooq Brohi', 'Farooq Nindwani', 'Awais Pirzado', 'Adil Brohi'];
 console.log("\t", st[0]);
 console.log("\t", st[1]);
 console.log("\t", st[2]);
